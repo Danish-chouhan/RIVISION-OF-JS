8d3fa0ef2b580ecaca53b13702047bb7d672336b
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-27 00:29:44 +0530

    Rivision

---
+++ RIVISION-OF-JS-VIDEOS/js/rivision.js
@@ -0,0 +1,15 @@
+// var a = 1
+// a = 11
+// var a = 111
+// document.write(a)
+
+// let a = 1
+// a = 11
+// // let a = 111
+// console.log(a)
+
+// const a =[1]
+// // a = 11
+// a[0]
+// // const a = 1
+// console.log(a)
