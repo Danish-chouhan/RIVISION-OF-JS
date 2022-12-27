13a591322cc7adb77d29b149fbc4ba7af71bd355
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-27 15:09:25 +0530

    Rivision

--- RIVISION-OF-JS-VIDEOS/js/rivision-2.js
+++ RIVISION-OF-JS-VIDEOS/js/rivision-2.js
@@ -1,3 +1,50 @@
 // var a = Math.ceil(1.1)
 // document.write(a)
 // it will give you upper value
+
+var a = Math.floor(-3.1)
+document.write(a)
+// it will give you downword value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// var a = Math.ceil(1.1)
+// document.write(a)
+// it will give you upper value
+
+// -5 -4 -3 -2 -1 0 1 2 3 4 5
