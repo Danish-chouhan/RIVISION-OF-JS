e5bbd13dd6f47fb9303fc6a0f46e21a81218f824
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-27 15:14:02 +0530

    Rivision

--- RIVISION-OF-JS-VIDEOS/js/rivision-2.js
+++ RIVISION-OF-JS-VIDEOS/js/rivision-2.js
@@ -2,13 +2,14 @@
 // document.write(a)
 // it will give you upper value
 
-var a = Math.floor(-3.1)
-document.write(a)
+// var a = Math.floor(-3.1)
+// document.write(a)
 // it will give you downword value
 
-// var a = Math.ceil(1.1)
-// document.write(a)
-// it will give you upper value
+var a = Math.round(1.4)
+document.write(a)
+// it will give you uperor downword value according to the center point if statement have lower value from 5 then it will give you downWord value and if the statement have uppervalue from 5 then it will give you upper value
+
 
 // var a = Math.ceil(1.1)
 // document.write(a)
