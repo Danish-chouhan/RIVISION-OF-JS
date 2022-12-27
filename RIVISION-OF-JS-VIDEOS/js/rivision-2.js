2ca5edca02ee1780ec3ebc7e3a448399b00daac1
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-27 15:19:24 +0530

    Rivision

--- RIVISION-OF-JS-VIDEOS/js/rivision-2.js
+++ RIVISION-OF-JS-VIDEOS/js/rivision-2.js
@@ -10,14 +10,13 @@
 // document.write(a)
 // it will give you uperor downword value according to the center point if statement have lower value from 5 then it will give you downWord value and if the statement have uppervalue from 5 then it will give you upper value
 
-
-var a = Math.trunc(1.99)
-document.write(a)
+// var a = Math.trunc(1.99)
+// document.write(a)
 // it will only return you the integers it will hide all dacimal value it means after dot all value are removed
 
-// var a = Math.ceil(1.1)
-// document.write(a)
-// it will give you upper value
+var a = Math.max(-1 , -2 , -3 , -4 , 0)
+document.write(a)
+// it will always return you the bigest value
 
 // var a = Math.ceil(1.1)
 // document.write(a)
